397b8164c0026afa6713a50acd975591139fe89e
Jens Van Ranst <jens.vanranst@gmail.com>
2012-09-16 14:42:35 +0200

    Finish layput and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,41 +2,41 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the right selector" do visit '/static_pages/home'
+    it "should have the right selector" do visit root_path
      page.should have_selector('title', :text => 'SampleApp')
     end 
   
 end
   describe "Help page" do
-    it "Should have content 'Help'" do visit '/static_pages/help' 
+    it "Should have content 'Help'" do visit help_path 
       page.should have_content('Help')
     end
     
     it "Should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => 'Help')
     end
   end
   
   describe "About page" do
-    it "Should have content 'About Us'" do visit '/static_pages/about' 
+    it "Should have content 'About Us'" do visit about_path 
       page.should have_content('About Us')
     end
     
      it "Should have the title 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => 'About us')
     end
     
   end
   
   describe "Contact page" do
-    it "Should have content 'Contact'" do visit '/static_pages/contact' 
+    it "Should have content 'Contact'" do visit contact_path
       page.should have_content('Contact')
     end
     
      it "Should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => 'Contact')
     end
     
